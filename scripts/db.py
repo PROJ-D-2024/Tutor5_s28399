9c03ad677e79f834dca7ccda39268cee75bf8564
sofeeka <sofiiahrychukh@gmail.com>
2024-11-05 01:21:00 +0100

    supported db update when handing na and duplicates

--- scripts/db.py
+++ scripts/db.py
@@ -42,10 +42,17 @@ def get_connection_string():
 
 def create_table(df, table_name):
     try:
+        df = prepare_data_for_create_table(df)
+
         engine = create_engine(get_connection_string())
 
         # create table
         df.to_sql(name=table_name, con=engine, index=False, if_exists='replace')
+
+        # set id
+        with engine.connect() as connection:
+            alter_table_sql = f'ALTER TABLE {table_name} ADD PRIMARY KEY (id)'
+            connection.execute(alter_table_sql)
 
         # read the data to see if it was created if needed
         # result_df = pd.read_sql(f'SELECT * FROM {table_name}', con=engine)
@@ -56,6 +63,65 @@ def create_table(df, table_name):
 
     except Exception as e:
         raise RuntimeError('unexpected error occurred') from e
+
+
+def prepare_data_for_create_table(df):
+
+    # adding unique id to the data
+    df['id'] = range(1, len(df) + 1)
+
+    # convert arrival date year, month and day of month into a datetime
+    df = df.rename(columns={
+        'arrival_date_year': 'year',
+        'arrival_date_month': 'month',
+        'arrival_date_day_of_month': 'day'
+    })
+
+    # months are written verbally, convert January to 1, February to 2 etc.
+    df['month'] = pd.to_datetime(df['month'], format='%B').dt.month
+    # aggregate all values into one column representing arrival date
+    df['arrival_date'] = pd.to_datetime(df[['year', 'month', 'day']])
+    # drop year month day and week, they are deprecated now
+    df.drop(columns={'year', 'month', 'day', 'arrival_date_week_number'}, inplace=True)
+
+    dtype_mapping = {
+        'hotel': 'category',
+        'is_canceled': 'bool',
+        'lead_time': 'int',
+        'adults': 'int',
+        'children': 'int',
+        'babies': 'int',
+        'meal': 'category',
+        'country': 'category',
+        'market_segment': 'category',
+        'distribution_channel': 'category',
+        'is_repeated_guest': 'bool',
+        'previous_cancellations': 'int',
+        'previous_bookings_not_canceled': 'int',
+        'reserved_room_type': 'category',
+        'assigned_room_type': 'category',
+        'booking_changes': 'int',
+        'deposit_type': 'category',
+        'agent': 'int',
+        'days_in_waiting_list': 'int',
+        'customer_type': 'category',
+        'adr': 'float',
+        'required_car_parking_spaces': 'int',
+        'total_of_special_requests': 'int',
+        'reservation_status': 'category',
+        'reservation_status_date': 'datetime64[ns]'
+    }
+
+    # correct datatype if it is different
+    for column, dtype in dtype_mapping.items():
+        if df[column].dtype == dtype:
+            continue
+        if dtype == 'datetime64[ns]':
+            df[column] = pd.to_datetime(df[column])
+        else:
+            df[column] = df[column].astype(dtype)
+
+    return df
 
 
 def load_dataset(table_name):
